a4f9a681552c71548ce109892d06b484bed5bf8a
DaniBonica001 <danielabc_0106@hotmail.com>
2024-09-03 11:27:12 -0500

    swagger

--- src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
+++ src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
@@ -4,6 +4,9 @@ import co.analisys.biblioteca.model.Email;
 import co.analisys.biblioteca.model.Usuario;
 import co.analisys.biblioteca.model.UsuarioId;
 import co.analisys.biblioteca.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +16,27 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(
+            summary = "Obtener información de un usuario",
+            description = "Este endpoint permite obtener la información de un usuario específico proporcionando su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado y devuelto exitosamente"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @GetMapping("/{id}")
     public Usuario obtenerUsuario(@PathVariable String id) {
         return usuarioService.obtenerUsuario(new UsuarioId(id));
     }
 
+    @Operation(
+            summary = "Cambiar el email de un usuario",
+            description = "Este endpoint permite cambiar el email de un usuario específico proporcionando su ID y el nuevo email."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email actualizado exitosamente"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @PutMapping("/{id}/email")
     public void cambiarEmail(@PathVariable String id, @RequestBody String nuevoEmail) {
         usuarioService.cambiarEmailUsuario(new UsuarioId(id), new Email(nuevoEmail));
